7380fa9716c71ccffd1859cd66077a0aed50497e
ffgtfgh <j.o.v.i.e.f.a.n.g.098@gmail.com>
2025-05-21 23:58:22 +0800

    fixed will possibly move to praw

--- meme_page.py
+++ meme_page.py
@@ -4,20 +4,20 @@ import json
 
 app = Flask(__name__)
 
-def get_meme(src:str="") -> tuple[str, str, str, str]:
-    url = "https://meme-api.com/gimme/" + src + '20'
+def get_meme(src:str="") -> tuple[str]:
+    url = "https://meme-api.com/gimme/" + src
     response = json.loads(request("GET", url).text)
-    if response.get("code") != -1 : 
-        return "", "", f"Error {response["code"]}, {response[message]}"
+    if response.get("code") != None : 
+        return "", "", f"Error {response["code"]}, {response["message"]}", ""
     if response["nsfw"]:
-        return get_meme()
+        return *get_meme(), "(Random meme/image from subreddit contained nsfw, returning random meme)"
     else: 
         return response["preview"][-2], response["subreddit"], response["title"], response["postLink"]
 
 @app.route('/<hmm>')
 def subreddit(hmm):
-    meme, subreddit, title, link = get_meme(hmm)
-    return render_template('index.html', meme=meme, subreddit=subreddit, title=title, link=link)
+    meme, subreddit, title, link, *extra = get_meme(hmm)
+    return render_template('index.html', meme=meme, subreddit=subreddit, title=title, link=link, extra=extra)
 
 @app.route('/')
 def index():
