2ad835ee0ba0c5b492ef49e70b76e81a7151165d
ffgtfgh <j.o.v.i.e.f.a.n.g.098@gmail.com>
2025-05-22 23:27:15 +0800

    update? idk what even happened

--- meme_page.py
+++ meme_page.py
@@ -1,38 +1,52 @@
-from flask import Flask, render_template, request
+from re import sub
+from flask import Flask, render_template, request, session
 from random import choice#, randint
 import praw
-from re import sub
+
 from pprint import pprint
 
 app = Flask(__name__)
 
+app.config.from_pyfile('config.py')
+
+def init():
+    session['src'] = ''
+    session['return_selfpost'] = False
+    session['nsfw'] = False
+    session['init'] = True
+
 reddit = praw.Reddit("main bot") #Requires praw.ini file which im not gonna share duh
-def get_meme_v2(src:str='',failed:int=0,return_selfpost:bool=False,nsfw:bool=False):
+
+def get_meme_v2(failed:int=0):
     #Ensures not empty subreddit field
-    if src == '' or failed > 6: src = choice(['memes','dankmemes','meirl','wholesomememes'] * 12
+    if session['src'] == '' or failed > 6: src = (['memes','dankmemes','meirl','wholesomememes'] * 12
                                 + ['historymemes', 'angryupvote', 'artmemes', 'programmerhumor', 'cursedcomments', 'meowirl', 'prequelmemes', 'unexpected'] * 6
                                 + ['shitposting','196','meme','youseecomrade']) #For a total of 100 options
-    for submission in reddit.subreddit(src).random_rising(limit=1):
+    elif type(session['src']) == str : src = session['src'].split('+')
+    #At this stage session['src'] will be a list of possible subreddits
+    subreddit_display_name = choice(src)
+    print(subreddit_display_name)
+    for submission in reddit.subreddit(subreddit_display_name).random_rising(limit=1):
 
-        if submission.over_18 and not nsfw:
+        if submission.over_18 and not session['nsfw']:
             print("\nNSFW\n")
-            return get_meme_v2(src, failed=failed+1)
+            return get_meme_v2(failed=failed+1)
 
 
-        elif submission.is_self and not return_selfpost: 
+        elif submission.is_self and not session['return_selfpost']: 
             print("\nisself\n")
-            return get_meme_v2(src, failed=failed+1)
+            return get_meme_v2(failed=failed+1)
 
 
         elif hasattr(submission, 'is_gallery'):
             print("\nisgallery\n")
-            return get_meme_v2(src)
+            return get_meme_v2()
             #idk how to implement gallery
             #Arrow keys???
             #besides gallery typically not memes
 
         common_kwargs = { #For unpacking in render_template
-                'subreddit':src, 
+                'subreddit':subreddit_display_name, 
                 'title':submission.title, 
                 'link':'https://reddit.com' + submission.permalink}
                 
@@ -40,7 +54,7 @@ def get_meme_v2(src:str='',failed:int=0,return_selfpost:bool=False,nsfw:bool=Fal
         if submission.is_video:
             #idk why im using fallback url but ok
             return render_template(
-                'video.html', 
+                'media/video.html', 
                 meme=submission.media['reddit_video']['fallback_url'], 
                 **common_kwargs)
 
@@ -52,36 +66,42 @@ def get_meme_v2(src:str='',failed:int=0,return_selfpost:bool=False,nsfw:bool=Fal
                     if match.group(1) == "width": return 'width = "70%"'
                     else: return 'height = "70%"'
                 return render_template(
-                    'embed.html',
+                    'media/embed.html',
                     embed=sub(r'(width|height)=\"\d+\"',resize,submission.media['oembed']['html']),
                     **common_kwargs)
 
         elif submission.is_self:
             return render_template(
-                "embed.html",
+                "media/embed.html",
                 embed=submission.selftext_html,
                 **common_kwargs
             )
         else:
             #if is image
             return render_template(
-                'index.html', 
+                'media/index.html', 
                 meme=submission.preview['images'][0]['resolutions'][-1]['url'], 
                 **common_kwargs
                 )
     
 
+
 @app.route('/',methods=["GET"])
 def index():
-    kwargs = {}
-    try: kwargs['src'] = request.args['subreddit']
-    except: pass
-    try: kwargs['return_selfpost'] = False if request.args["return_selfpost"].lower() == "false" else True
-    except: pass
-    try: kwargs['nsfw'] = False if request.args["plsplsplsimdesperateshowmensfw"].lower() == "false" else True
-    except: pass
-    return get_meme_v2(**kwargs)
+    if 'init' not in session: init()
+    passed = 0
+    try: session['src'] = request.args['subreddit']
+    except: passed += 1
+    try: session['return_selfpost'] = True if request.args["return_selfpost"].lower() == "true" else False
+    except: passed += 1
+    try: session['nsfw'] = True if request.args["plsplsplsimdesperateshowmensfw"].lower() == "true" else False
+    except: passed += 1
+    if passed < 3: app.redirect('/')
+    return get_meme_v2()
 
+@app.route('/',methods=["POST"])
+def settings():
+    return render_template('settings/settings')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080, debug=True)
