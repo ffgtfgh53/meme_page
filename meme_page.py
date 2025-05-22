4ab009a9fea46a5137f9513459a3ffbe5e1c21d0
ffgtfgh <j.o.v.i.e.f.a.n.g.098@gmail.com>
2025-05-22 14:47:46 +0800

    update

--- meme_page.py
+++ meme_page.py
@@ -2,39 +2,42 @@ from flask import Flask, render_template, request
 from random import choice#, randint
 import praw
 from re import sub
+from pprint import pprint
 
 app = Flask(__name__)
 
 reddit = praw.Reddit("main bot") #Requires praw.ini file which im not gonna share duh
-def get_meme_v2(src:str=''):
+def get_meme_v2(src:str='',failed:int=0):
     #Ensures not empty subreddit field
-    if src == '': src = choice(['memes','dankmemes','meirl','wholesomememes'] * 12
+    if src == '' or failed > 6: src = choice(['memes','dankmemes','meirl','wholesomememes'] * 12
                                 + ['historymemes', 'angryupvote', 'artmemes', 'programmerhumor', 'cursedcomments', 'meowirl', 'prequelmemes', 'unexpected'] * 6
                                 + ['shitposting','196','meme','youseecomrade']) #For a total of 100 options
-    subreddit = reddit.subreddit(src)
-    #magic_number = randint(0, 12)
-    for i, submission in enumerate(subreddit.random()):
+    for submission in reddit.subreddit(src).random_rising(limit=1):
 
         if submission.over_18:
-            return get_meme_v2()
+            print("\n\n\nNSFW\n\n\n")
+            return get_meme_v2(src, failed=failed+1)
             #No error message?
+
+        elif submission.is_self: 
+            print("\n\n\nisself\n\n\n")
+            return get_meme_v2(src, failed=failed+1)
+            #self explanatory, we cant display selfposts
+
+        elif hasattr(submission, 'is_gallery'):
+            print("\n\n\nisgallery\n\n\n")
+            return get_meme_v2(src)
+            #idk how to implement gallery
+            #Arrow keys???
+            #besides gallery typically not memes
 
         common_kwargs = { #For unpacking in render_template
                 'subreddit':src, 
                 'title':submission.title, 
                 'link':'https://reddit.com' + submission.permalink}
                 
-        if submission.is_self: 
-            return get_meme_v2(src)
-            #self explanatory, we cant display selfposts
 
-        elif hasattr(submission, 'is_gallery'):
-            return get_meme_v2(src)
-            #idk how to implement gallery
-            #Arrow keys???
-            #besides gallery typically not memes
-
-        elif submission.is_video:
+        if submission.is_video:
             #idk why im using fallback url but ok
             return render_template(
                 'video.html', 
@@ -46,7 +49,7 @@ def get_meme_v2(src:str=''):
                 #Is embed :cry:
                 def resize(match):
                     "used for re.sub() to change width and height of embed"
-                    if match.group(1) == "width": return 'width = "90%"'
+                    if match.group(1) == "width": return 'width = "70%"'
                     else: return 'height = "70%"'
                 return render_template(
                     'embed.html',
@@ -57,10 +60,9 @@ def get_meme_v2(src:str=''):
             #if is image
             return render_template(
                 'index.html', 
-                meme=submission.url, 
+                meme=submission.preview['images'][0]['resolutions'][-1]['url'], 
                 **common_kwargs
                 )
-    return get_meme_v2()
     
 
 @app.route('/',methods=["GET"])
@@ -69,4 +71,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0',port=8080, debug=True)
