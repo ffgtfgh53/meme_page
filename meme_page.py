9bba4d9715d9588e38f913f3b9f72b8f096307fa
ffgtfgh <j.o.v.i.e.f.a.n.g.098@gmail.com>
2025-05-22 12:06:24 +0800

    Switched to praw, works better Support for videos now yay Does not support gallery yet and may break with small subreddits

--- meme_page.py
+++ meme_page.py
@@ -1,28 +1,71 @@
-from flask import Flask, render_template
-from requests import request
-import json
+from flask import Flask, render_template, request
+from requests import request as webrequest
+from random import randint, choice
+import json, praw
+from pprint import pprint
+from re import sub, Match
 
 app = Flask(__name__)
 
-def get_meme(src:str="") -> tuple[str]:
-    url = "https://meme-api.com/gimme/" + src
-    response = json.loads(request("GET", url).text)
-    if response.get("code") != None : 
-        return "", "", f"Error {response["code"]}, {response["message"]}", ""
-    if response["nsfw"]:
-        return *get_meme(), "(Random meme/image from subreddit contained nsfw, returning random meme)"
-    else: 
-        return response["preview"][-2], response["subreddit"], response["title"], response["postLink"]
+reddit = praw.Reddit("main bot") #Requires praw.ini file which im not gonna share duh
+def get_meme_v2(src:str=''):
+    print(src)
+    if src == '': src = choice(['memes','dankmemes','meirl']*9+['shitposting','196','meme'])
+    print(src)
+    print(src)
+    subreddit = reddit.subreddit(src)
+    magic_number = randint(0, 12)
+    for i, submission in enumerate(subreddit.random_rising(limit=16)):
+        if i != magic_number: 
+            continue
+        if submission.over_18:
+            magic_number += 1
+            continue #Will be at least 2 chances to get non NSFW post
+        if submission.is_self: 
+            return get_meme_v2(src)
+            #self explanatory, we cant display selfposts
+        elif hasattr(submission, 'is_gallery'):
+            return get_meme_v2(src)
+            #idk how to implement gallery that looks nice
+            #besides gallery typically not memes
+        elif submission.is_video:
+            #idk why im using fallback but ok
+            return render_template(
+                'video.html', 
+                meme=submission.media['reddit_video']['fallback_url'], 
+                subreddit=src, 
+                title=submission.title, 
+                link='https://reddit.com' + submission.permalink
+                )
+        elif submission.media != None:
+            if submission.media.get('oembed', False):
+                def resize(match:Match):
+                    if match.group(1) == "width": return 'width = "90%"'
+                    else: return 'height = "70%"'
+                return render_template(
+                    'embed.html',
+                    embed=sub(r'(width|height)=\"\d+\"',resize,submission.media['oembed']['html']),
+                    subreddit=src,
+                    title=submission.title,
+                    link='https://reddit.com' + submission.permalink
+            )
+        else:
+            #if is image
+            return render_template(
+                'index.html', 
+                meme=submission.url, 
+                subreddit=src, 
+                title=submission.title, 
+                link='https://reddit.com' + submission.permalink
+                )
+    print("ran out??")
+    return get_meme_v2()
+    
 
-@app.route('/<hmm>')
-def subreddit(hmm):
-    meme, subreddit, title, link, *extra = get_meme(hmm)
-    return render_template('index.html', meme=meme, subreddit=subreddit, title=title, link=link, extra=extra)
-
-@app.route('/')
+@app.route('/',methods=["GET"])
 def index():
-    meme, subreddit, title, link = get_meme()
-    return render_template('index.html', meme=meme, subreddit=subreddit, title=title, link=link)
+    print(request.args)
+    return get_meme_v2(request.args.get("subreddit", ""))
 
 
 if __name__ == "__main__":
